d67c3d144fee8ee899dcdea42074131114630aad
jbslanka <john.slankas@ncsu.edu>
2023-05-29 22:11:30 -0400

    exif service updated to Java 17 and latest libraries

--- MicroServices/service_image_exif/src/test/java/edu/ncsu/las/image/api/MyResourceTest.java
+++ MicroServices/service_image_exif/src/test/java/edu/ncsu/las/image/api/MyResourceTest.java
@@ -1,11 +1,11 @@
 package edu.ncsu.las.image.api;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.client.ClientConfig;
